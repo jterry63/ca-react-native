7ffa34ffb6c5ed62db6fa3a27dc761ea2c24d033
jterry <jordan.terry@mx.com>
2019-04-06 23:30:59 -0600

    finished auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
-import { View, Text } from 'react-native';
-import { Header } from './components/common';
+import { View } from 'react-native';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm'
 
 
 class App extends Component {
+    state = { loggedIn: null }
+
     componentWillMount() {
         firebase.initializeApp({
             apiKey: "AIzaSyBFVFx_WPZzA2uQUP3zYcNp3Wtcn_HJROM",
@@ -15,15 +17,41 @@ class App extends Component {
             storageBucket: "auth-523a5.appspot.com",
             messagingSenderId: "142504376049"
           });
-    }
-    render() {
-        return (
-            <View>
-                <Header headerText='Authentication' />
-                <LoginForm />
-            </View>
-        );
-    }
-}
 
-export default App;
+          firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+              this.setState({ loggedIn: true });
+            } else {
+              this.setState({ loggedIn: false });
+            }
+          });
+        }
+      
+        renderContent() {
+          switch (this.state.loggedIn) {
+            case true:
+              return (
+                <CardSection>
+                    <Button onPress={() => firebase.auth().signOut()}>
+                    Log Out
+                    </Button>
+                </CardSection>
+              );
+            case false:
+              return <LoginForm />;
+            default:
+              return <Spinner size="large" />;
+          }
+        }
+      
+        render() {
+          return (
+            <View>
+              <Header headerText="Authentication" />
+              {this.renderContent()}
+            </View>
+          );
+        }
+      }
+      
+      export default App;
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -16,22 +16,21 @@ const Header = (props) => {
 
 const styles = {
     viewStyle: {
-        backgroundColor: '#F8F8F8',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 120,
-        paddingTop: 15,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.2,
-        elevation: 2,
-        position: 'relative'
+      backgroundColor: '#F8F8F8',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 120,
+      paddingTop: 15,
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.2,
+      elevation: 2,
+      position: 'relative'
     },
     textStyle: {
-        fontSize: 20, 
-        
+      fontSize: 20
     }
-}
+  };
 
 //make the component available to other parts of the app
 
